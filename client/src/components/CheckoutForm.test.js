d771012f3a825e14d60159b72ae47999340102b5
ndossett <niki.m.dossett@gmail.com>
2021-01-15 14:23:37 -0500

    added extra test

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -5,8 +5,15 @@ import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
+test("renders CheckoutForm without errors", ()=>{
+    render(<CheckoutForm />)
+})
+
 test("form header renders", () => {
     render(<CheckoutForm />)
+
+    const header = screen.queryByText("Checkout Form")
+    expect(header).toBeInTheDocument()
 });
 
 test("form shows success message on submit with form details", async () => {
